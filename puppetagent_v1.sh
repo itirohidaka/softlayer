3fa63d9205bd4ad039dbff2da23a04681f94a45f
Itiro <itirohidaka@msn.com>
2015-04-27 18:38:04 -0300

    Change Puppet Master IP
    Change Puppet Master IP

--- puppetagent_v1.sh
+++ puppetagent_v1.sh
@@ -19,8 +19,9 @@ sed -i "s/ubuntu/ubuntu_sl_01/g" /etc/hostname
 hostname ubuntu_sl_01
 echo "2.1 End of Step 2.1 - Initial Configuration - Hostname: $?" >> $LOG_FILE
 
-# configuração do arquivo hosts 
+# configuração do arquivo hosts
 #sed -i "s/ubuntu/ubuntu_sl_01/g" /etc/hosts
+# Substitute "127.0.0.1" with the Puppet Master IP Address
 sed -i '$ a\127.0.0.1 puppet' /etc/hosts
 echo "2.1 End of Step 2.1 - Initial Configuration - Hosts File: $?" >> $LOG_FILE
 
@@ -38,7 +39,7 @@ echo "3. End of Linux Update: $?" >> $LOG_FILE
 apt-get -y install curl
 apt-get -y install openssh-server
 echo "4. End of Pre-requisites installations for Puppet Agent: $?" >> $LOG_FILE
- 
+
 #=======================================================================
 # 5. Instalação do Puppet Agent
 #=======================================================================
