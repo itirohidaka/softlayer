e9d46137623aa92c45669aeaafd05ae75ca984ab
Itiro <itirohidaka@msn.com>
2015-06-07 10:07:49 -0300

    abcde

--- puppetagent_v2.sh
+++ puppetagent_v2.sh
@@ -14,17 +14,9 @@ LOG_FILE=/var/log/softlayer_bootstrap.log
 # 2. Initial Configuration
 #=======================================================================
 sudo su -
-# configuração do arquivo hostname
-sed -i "s/ubuntu/ubuntu_sl_01/g" /etc/hostname
-hostname ubuntu_sl_01
-echo "2.1 End of Step 2.1 - Initial Configuration - Hostname: $?" >> $LOG_FILE
 
 # configuração do arquivo hosts
-#sed -i "s/ubuntu/ubuntu_sl_01/g" /etc/hosts
-# Substitute "127.0.0.1" with the Puppet Master IP Address
 sed -i '$ a\169.54.65.91 puppet' /etc/hosts
-echo "2.1 End of Step 2.1 - Initial Configuration - Hosts File: $?" >> $LOG_FILE
-
 echo "2. End of Step 2 - Initial Configuration: $?" >> $LOG_FILE
 
 #=======================================================================
@@ -47,11 +39,10 @@ apt-get -y install puppet
 echo "4. End of Puppet Agent installation: $?" >> $LOG_FILE
 
 # configuração do arquivo puppet.conf
-#sed -i "s/ubuntu/ubuntu_sl_01/g" /etc/hosts
-# Substitute "127.0.0.1" with the Puppet Master IP Address
 sed -i '/templatedir=/a runinterval=60' /etc/puppet/puppet.conf
 echo "5.1 End of Step 5.1 - Puppet runinterval: $?" >> $LOG_FILE
 
+# configuração do arquivo puppet.conf
 service puppet restart
 echo "5.2 End of Step 5.2 - Puppet restart: $?" >> $LOG_FILE
 puppet agent --enable
