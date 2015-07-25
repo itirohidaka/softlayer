0e5ca76eb6d2edabb4d7b7c95267390498f8c15f
Itiro <itirohidaka@msn.com>
2015-07-25 00:58:17 -0300

    Create node-red.sh
    first creation

---
+++ node-red.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#=======================================================================
+# File: nodered.sh
+# Description: nodered instalation for CentOS
+# Author: Gerson Itiro Hidaka
+# Date: 25/07/2015 (dd/mm/yyy)
+#==============================================================================
+# Disclaimer
+# Be aware that all scripts are run at your own risk and while every script has
+# been written with the intention of minimising the potential for unintended
+# consequences, the owners, hosting providers and contributers cannot be held
+# responsible for any misuse or script problems.
+#==============================================================================
+
+#=======================================================================
+# 1. Variables
+#=======================================================================
+LOG_FILE=/var/log/nodered.log
+
+#=======================================================================
+# 2. Initial Configuration
+#=======================================================================
+sudo su -
+echo "2. End of Step 2 - Initial Configuration: $?" >> $LOG_FILE
+
+#=======================================================================
+# 3. CentOS Update
+#=======================================================================
+yum -y update
+echo "3. End of Linux Update: $?" >> $LOG_FILE
+
+#=======================================================================
+# 4. Pre-requisites for Node-Red (Node.js)
+#=======================================================================
+curl --silent --location https://rpm.nodesource.com/setup | bash -
+sudo yum -y install nodejs
+sudo yum -y install gcc-c++ make
+echo "4. End of Pre-requisites installations for Node-Red: $?" >> $LOG_FILE
+
+#=======================================================================
+# 5. Node-Red Installation
+#=======================================================================
+sudo npm install -g node-red
+echo "5. End of Node-Red installation: $?" >> $LOG_FILE
+
+#=======================================================================
+# 6. Node-Red Execution
+#=======================================================================
+nohup node-red &
+echo "6. End of Node-Red execution: $?" >> $LOG_FILE
+
+#=======================================================================
+# End.
+#=======================================================================
+echo "End of Script...: $?" >> $LOG_FILE
